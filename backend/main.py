2c55aaa2a33211d5527a2bb3010228b75375d2af
CITIZEN-OF-INDIA <jyoti9313321050@gmail.com>
2026-06-02 17:29:16 +0530

    NEW COMMIT

--- backend/main.py
+++ backend/main.py
@@ -7,15 +7,14 @@ app = FastAPI(
     title="RasgullaAI"
 )
 
-app.add_middleware(
+    app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(chat_router)
 
